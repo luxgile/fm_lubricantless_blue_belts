fac0c26b2bf03a0b6a6f60e459f09a871ec51561
luxgile <bisslit.12.2@gmail.com>
2024-11-25 21:23:06 +0100

    Initial commit

---
+++ data.lua
@@ -0,0 +1,4 @@
+data.raw.recipe["express-transport-belt"].ingredients = {
+  { type = "item", name = "iron-gear-wheel",     amount = 10 },
+  { type = "item", name = "fast-transport-belt", amount = 1 },
+}
