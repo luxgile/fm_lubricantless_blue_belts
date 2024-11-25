50e4090fef168b0751c69242335f29d737fa0229
luxgile <bisslit.12.2@gmail.com>
2024-11-25 23:30:33 +0100

    Updated to v0.1.1

--- data.lua
+++ data.lua
@@ -2,3 +2,14 @@ data.raw.recipe["express-transport-belt"].ingredients = {
   { type = "item", name = "iron-gear-wheel",     amount = 10 },
   { type = "item", name = "fast-transport-belt", amount = 1 },
 }
+
+data.raw.recipe["express-underground-belt"].ingredients = {
+  { type = "item", name = "iron-gear-wheel",       amount = 80 },
+  { type = "item", name = "fast-underground-belt", amount = 2 },
+}
+
+data.raw.recipe["express-splitter"].ingredients = {
+  { type = "item", name = "fast-splitter",    amount = 1 },
+  { type = "item", name = "iron-gear-wheel",  amount = 10 },
+  { type = "item", name = "advanced-circuit", amount = 10 },
+}
